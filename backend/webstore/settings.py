ed8de6aa702972997b177f9fbd62d2a8b0afdb11
Przemek <przemek@192.168.1.16>
2025-09-13 00:58:25 +0200

    Configure PostgreSQL database

--- backend/webstore/settings.py
+++ backend/webstore/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'webstore.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
