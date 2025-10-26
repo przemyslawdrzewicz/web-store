edc19e8ec7ce262b2f8a93f2d893a7f2779cfa5e
Przemek <przemek@192.168.1.11>
2025-10-26 09:38:58 +0100

    Create Product model

--- backend/products/models.py
+++ backend/products/models.py
@@ -3,6 +3,10 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100, blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    stock = models.PositiveIntegerField(blank=True, null=True)
+    image = models.ImageField(upload_to='products/', blank=True, null=True) 
     
     def __str__(self):
         return self.name
--- backend/webstore/settings.py
+++ backend/webstore/settings.py
@@ -30,6 +30,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Application definition
 
